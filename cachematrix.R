c20b2008f4571730da97520b7fb11b62560eb426
Jacky <kuoyichun1102@gmail.com>
2014-12-14 02:54:21 +0800

    add comment

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,8 @@
 ## Put comments here that give an overall description of what your
 ## functions do
-
 ## Write a short comment describing this function
+#implement makeCacheMatrux by copying the form of makeVector function in example
+#return value is a list consisting of 4 function 
 
 makeCacheMatrix <- function(x = matrix()) {
         s<-NULL
@@ -18,6 +19,8 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## Write a short comment describing this function
+#if cacheSolve has been executed, print a message "getting cached data"
+#otherwise, compute the inverse matrix of x, and store it
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
